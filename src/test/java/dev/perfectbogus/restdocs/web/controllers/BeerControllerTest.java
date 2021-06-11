e6272609860574e9e3d1833f7a0ccedeedf6383e
eduardo escobar <war_army_10@hotmail.com>
2021-06-11 14:18:32 -0500

    Step 93: Documenting Responses

--- src/test/java/dev/perfectbogus/restdocs/web/controllers/BeerControllerTest.java
+++ src/test/java/dev/perfectbogus/restdocs/web/controllers/BeerControllerTest.java
@@ -24,6 +24,8 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
@@ -56,8 +58,18 @@ class BeerControllerTest {
                         ),
                         requestParameters(
                                 parameterWithName("iscold").description("is beer cold query param")
-                        )
-                        ));
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("id of beer"),
+                                fieldWithPath("version").description("Version number"),
+                                fieldWithPath("createdDate").description("Date Created"),
+                                fieldWithPath("lastModifiedDate").description("Date Updated"),
+                                fieldWithPath("beerName").description("Beer Name"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("UPC of Beer"),
+                                fieldWithPath("price").description("Price"),
+                                fieldWithPath("quantityOnHand").description("Quantity on Hand")
+                        )));
     }
 
     @Test
